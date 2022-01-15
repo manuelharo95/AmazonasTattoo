7546da2d987acbac2948831eb104608f95ac6b63
manuelharofulgencio@hotmail.com <manuelharofulgencio@hotmail.com>
2022-01-15 04:00:34 +0100

    delete menu.js
    delete menu.js

--- js/menu.js
+++
@@ -1,5 +0,0 @@
-const menu = () => {
-  document.querySelector(".header__navbar").classList.toggle("active")
-    ? document.querySelector(".header__navbar").style.display = "block"
-    : document.querySelector(".header__navbar").style.display = "none";
-};
